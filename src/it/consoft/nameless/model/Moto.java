21948eddf25c3135af5e826734ea80f5a091974f
nameless24 <stefano92fred@gmail.com>
2018-03-07 17:36:05 +0100

    Classi moto e moto composta

---
+++ src/it/consoft/nameless/model/Moto.java
@@ -0,0 +1,133 @@
+package it.consoft.nameless.model;
+
+public class Moto {
+	private int id;
+	
+	private String marca;
+	
+	private String modello;
+	
+	private int cilindrata;
+	
+	private float cavalli;
+	
+	private float velocita;
+	
+	private String colore;
+	
+	private String tipo;
+	
+	private float accelerazione;
+	
+	private float manovrabilita;
+	
+	private float frenata;
+	
+	private float prezzo;
+	
+	private boolean antifurto;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModello() {
+		return modello;
+	}
+
+	public void setModello(String modello) {
+		this.modello = modello;
+	}
+
+	public int getCilindrata() {
+		return cilindrata;
+	}
+
+	public void setCilindrata(int cilindrata) {
+		this.cilindrata = cilindrata;
+	}
+
+	public float getCavalli() {
+		return cavalli;
+	}
+
+	public void setCavalli(float cavalli) {
+		this.cavalli = cavalli;
+	}
+
+	public float getVelocita() {
+		return velocita;
+	}
+
+	public void setVelocita(float velocita) {
+		this.velocita = velocita;
+	}
+
+	public String getColore() {
+		return colore;
+	}
+
+	public void setColore(String colore) {
+		this.colore = colore;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public float getAccelerazione() {
+		return accelerazione;
+	}
+
+	public void setAccelerazione(float accelerazione) {
+		this.accelerazione = accelerazione;
+	}
+
+	public float getManovrabilita() {
+		return manovrabilita;
+	}
+
+	public void setManovrabilita(float manovrabilita) {
+		this.manovrabilita = manovrabilita;
+	}
+
+	public float getFrenata() {
+		return frenata;
+	}
+
+	public void setFrenata(float frenata) {
+		this.frenata = frenata;
+	}
+
+	public float getPrezzo() {
+		return prezzo;
+	}
+
+	public void setPrezzo(float prezzo) {
+		this.prezzo = prezzo;
+	}
+
+	public boolean isAntifurto() {
+		return antifurto;
+	}
+
+	public void setAntifurto(boolean antifurto) {
+		this.antifurto = antifurto;
+	}
+}
---
+++ src/it/consoft/nameless/model/MotoComposta.java
@@ -0,0 +1,13 @@
+package it.consoft.nameless.model;
+
+import java.util.List;
+
+public class MotoComposta {
+	private int id;
+	
+	private int idUtente;
+	
+	private int idMoto;
+	
+	private List<Integer> listComponenti;
+}
