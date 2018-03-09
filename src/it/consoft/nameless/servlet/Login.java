e087d90012efbc17aa0b02ed00cc806c0c472953
client <client@DESKTOP-2EDKOJ8.station>
2018-03-09 10:17:32 +0100

    servlet login

---
+++ src/it/consoft/nameless/servlet/Login.java
@@ -0,0 +1,67 @@
+package it.consoft.nameless.servlet;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import it.consoft.nameless.model.User;
+import it.consoft.nameless.service.UserService;
+import it.consoft.nameless.service.UserServiceImpl;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	UserService userService = new UserServiceImpl();
+
+	private static final Logger logger = Logger.getLogger(Login.class.getName());
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Login() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.getRequestDispatcher("login.jsp").forward(request, response);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		User user = new User();
+		user.setUsername(username);
+		user.setPassword(password);
+		
+		User userDB = userService.getUserByUsernameAndPassword(username, password);
+		if(userDB != null) {
+		HttpSession session = request.getSession();
+		session.setAttribute("logged", true);
+		session.setAttribute("user", userDB);
+		logger.info("logged user: " + user.getUsername());
+		response.sendRedirect("listContacts");
+		}else {response.sendRedirect("loginerror=true");
+	}
+	}
+}
