5e23c8557737a2e247d491ac392901c78acea0c9
Budha7 <francescopesoli63@gmail.com>
2018-03-09 11:45:47 +0100

    Aggiunta la classe Componente

--- src/it/consoft/nameless/model/Componente.java
+++ src/it/consoft/nameless/model/Componente.java
@@ -4,21 +4,39 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "componente")
 public class Componente {
- 
+
 	@Id
 	@GeneratedValue
+	@Column(name = "id")
 	int idcomp;
 
+	@Column(name = "nome")
 	private String nome;
+
+	@Column(name = "marca")
 	private String marca;
+
+	@Column(name = "velocita")
 	private float velocita;
+
+	@Column(name = "frenata")
 	private float frenata;
+
+	@Column(name = "manovrabilita")
 	private float manovrabilita;
+
+	@Column(name = "accellerazione")
 	private float accellerazione;
+
+	@Column(name = "prezzo")
 	private float prezzo;
+
+	@Column(name = "antifurto")
 	private boolean antifurto;
 
 	public int getIdcomp() {
