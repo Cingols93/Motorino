57b75860dfa039260dc12aab0e301544b8de9b37
Cingo <Cingo@DESKTOP-6L9CDEM.station>
2018-03-14 15:06:40 +0100

    fix pulsante ordini navbar

--- src/it/consoft/nameless/servlet/OrdiniServlet.java
+++ src/it/consoft/nameless/servlet/OrdiniServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class OrdiniServlet
  */
-@WebServlet(value = "/ordine")
+@WebServlet(value = "/ordini")
 public class OrdiniServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
